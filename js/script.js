7e347d8882c6a3b135203146ee49e067d225b94f
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-15 20:14:18 +0000

    one more tiny change (@_@)

--- js/script.js
+++ js/script.js
@@ -25,14 +25,14 @@ function convertClicked() {
   //process
   if (angleOne == angleTwo && angleTwo == angleThree) {
     document.getElementById("type-of-triangle").innerHTML = 
-      "The triangle is an equilatoral triangle because all three angles are equal!"
+      "The triangle is an equilatoral triangle because all three angles are equal !"
   }
   else if ((angleOne == angleTwo) || (angleOne == angleThree) || (angleTwo == angleThree)){
      document.getElementById("type-of-triangle").innerHTML = 
-    "The triangle is an isosceles triangle because two angles are equal!"
+    "The triangle is an isosceles triangle because two angles are equal !"
   }
   else {
      document.getElementById("type-of-triangle").innerHTML = 
-    "The triangle is a scalene triangle becuase  no angles are equal!"
+    "The triangle is a scalene triangle becuase  no angles are equal !"
   }
 }
