dd7e5e057b7d29eba434558064ab7fe9d567920a
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-15 20:10:55 +0000

    made drastic changes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Emmanuel Fofeyin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emmanuel.Fofeyin
+// Created on: April 2022
 // This file contains the JS functions for index.html
 
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/github-page-pwa/sw.js", {
-    scope: "/github-page-pwa/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-05-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-05-HTML/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function gets the users input and converts the value to celsius
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function convertClicked() {
+  //input
+  const angleOne = parseInt(document.getElementById("angle-one").value)
+  const angleTwo = parseInt(document.getElementById("angle-two").value)
+  const angleThree = parseInt(document.getElementById("angle-three").value)
+
+  //process
+  if (angleOne == angleTwo && angleTwo == angleThree) {
+    document.getElementById("type-of-triangle").innerHTML = 
+      "The triangle is an equilatoral triangle because all three angles are equal!"
+  }
+  else if ((angleOne == angleTwo) || (angleOne == angleThree) || (angleTwo == angleThree)){
+     document.getElementById("type-of-triangle").innerHTML = 
+    "The triangle is an isosceles triangle because two angles are equal!"
+  }
+  else {
+     document.getElementById("type-of-triangle").innerHTML = 
+    "The triangle is a scalene triangle becuase  no angles are equal!"
+  }
 }
