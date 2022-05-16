38b5aedca6528f68ed6fd85916fd393a6f3ba8f0
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-05-16 00:39:04 +0000

    yy

--- js/script.js
+++ js/script.js
@@ -7,3 +7,32 @@
 /**
  * Check servie worker.
  */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-05-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-05-HTML/",
+  })
+}
+
+/**
+ * This function gets the users input and converts the value to celsius
+ */
+function convertClicked() {
+  //input
+  const angleOne = parseInt(document.getElementById("angle-one").value)
+  const angleTwo = parseInt(document.getElementById("angle-two").value)
+  const angleThree = parseInt(document.getElementById("angle-three").value)
+
+  //process
+  if (angleOne == angleTwo && angleTwo == angleThree) {
+    document.getElementById("type-of-triangle").innerHTML =
+      "The triangle is an equilatral triangle because all three angles are equal !"
+  }
+  else if ((angleOne == angleTwo) || (angleOne == angleThree) || (angleTwo == angleThree)){
+     document.getElementById("type-of-triangle").innerHTML =
+    "The triangle is an isosceles triangle because two angles are equal !"
+  }
+  else {
+     document.getElementById("type-of-triangle").innerHTML =
+    "The triangle is a scalene triangle becuase  no angles are equal !"
+  }
+}
